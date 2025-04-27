389adb74d9caacad853e9657cffc5770d41d1ffd
Lakki ali <lakki0072022@gmail.com>
2025-04-28 01:31:29 +0530

    change

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+ 
+
+        <nav>
+            
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import Navbar from "../../components/Navbar";
 
 export default function Home() {
   return (
-<></>
+    <>
+      <Navbar />
+      <h1>Hello, Next.js 15 App Router!</h1>
+    </>
   );
 }
