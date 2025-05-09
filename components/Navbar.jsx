a1dbdee5d0e315d2f7142af197de2489453712f0
Lakki ali <lakki0072022@gmail.com>
2025-05-09 12:51:55 +0530

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,23 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <nav>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          </ul>
-        </div>
+ 
 
-        <div>
-          <button></button>
-        </div>
+        <nav>
             
         </nav>
     </>
