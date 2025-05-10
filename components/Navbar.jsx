f0132b3715495d547814589ebcde211961769545
Lakki ali <lakki0072022@gmail.com>
2025-05-10 13:00:56 +0530

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,6 +9,20 @@ const Navbar = () => {
         <div>
           <h1 className='text'>Lucky <spam> A.</spam> </h1>
         </div>
+
+        <div className='nav-list'/>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Project</a></li>
+             <li><a href="#">Content</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <div/>
+
+          <div>
+            <button className='btn'>icon</button>
+          </div>
             
         </nav>
     </>
