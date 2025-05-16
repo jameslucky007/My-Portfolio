7dc21af462346e609198fff739e812cbceecfe8c
Lakki ali <lakki0072022@gmail.com>
2025-05-16 11:30:58 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
 <>
-  <h1 className="bg-blue-500 font-">hello world</h1>
+  <h1 className="bg-blue-500 text-9xl">hello world</h1>
+  <h5>It is a new begining</h5>
 </>
   );
 }
