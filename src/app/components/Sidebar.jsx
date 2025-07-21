4076614183ebb42c2cfc97a3aa4fd697de574204
Lucky A <105838230+jameslucky007@users.noreply.github.com>
2025-07-21 17:27:55 +0530

    changes

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaGithub, FaLinkedin   } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
@@ -7,21 +8,31 @@ const Sidebar = () => {
         <aside className=" max-w-auto p-10 ml-10 fixed h-screen  text-white">
       <img className='rounded-full w-42 h-42 object-cover justify-start  border-3 mb-8 border-amber-50'  src="https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="img" />
         <h1 className="text-4xl font-bold">Lucky A</h1>
-        <h3 className="text-xl mt-2">Web Developer</h3>
-        <p className="mt-4 text-gray-400">
-          I build accessible, pixel-perfect digital experiences for the web.
-        </p>
+        <h4 className="text-xl mt-2">Web Developer | Software Engineer </h4>
+        <p className="mt-4 text-gray-400 text-s pr-4">I build accessible, pixel-perfect digital <br /> experiences for the web.</p>
 
-        <nav className="mt-10 space-y-3">
-          <a href="#about" className="block text-sm text-gray-300">ABOUT</a>
+        <nav className="mt-10 space-y-3 ">
+          <a href="#about" className="block text-sm text-gray-300 ">  ABOUT</a>
           <a href="#experience" className="block text-sm text-gray-300">EXPERIENCE</a>
           <a href="#projects" className="block text-sm text-gray-300">PROJECTS</a>
+          <a href="#projects" className="block text-sm text-gray-300">CONTENT</a>
         </nav>
 
         <div className="mt-10 flex space-x-4">
-          <a href="#"><img src="/github.svg" alt="GitHub" className="w-5" /></a>
-          <a href="#"><img src="/linkedin.svg" alt="LinkedIn" className="w-5" /></a>
-          {/* Add more icons */}
+          <a href="#"><FaGithub  /></a>
+          <a href="#"><FaLinkedin /></a>
+          <a href="#"><FaLinkedin /></a>
+          <a href="#"><FaLinkedin /></a>
+          <a href="#"><FaLinkedin /></a>
+         
+        </div>
+        <div className="mt-5 flex space-x-4">
+          <a href="#"><FaGithub  /></a>
+          <a href="#"><FaLinkedin /></a>
+          <a href="#"><FaLinkedin /></a>
+          <a href="#"><FaLinkedin /></a>
+          <a href="#"><FaLinkedin /></a>
+         
         </div>
       </aside>
        
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   const userName = "Lucky";
-  const mainTitle = "Welcome to the Hero Section";
+  const mainTitle = "My Portfolio Website";
 
   return (
     <>
