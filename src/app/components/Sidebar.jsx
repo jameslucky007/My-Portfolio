54823689e9efd249710f916c75870b358c90caa2
Lucky A <105838230+jameslucky007@users.noreply.github.com>
2025-07-21 17:02:36 +0530

    changes

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -4,10 +4,10 @@ const Sidebar = () => {
   return (
     <>
        
-        <aside className=" max-w-auto p-10 fixed h-screen  text-white">
-      <img className='rounded-4xl w-50 justify-center ml-20' src="https://images.unsplash.com/photo-1750969393822-36e48a31895f?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="img" />
-        <h1 className="text-4xl font-bold">Brittany Chiang</h1>
-        <h2 className="text-xl mt-2">Front End Engineer</h2>
+        <aside className=" max-w-auto p-10 ml-10 fixed h-screen  text-white">
+      <img className='rounded-full w-42 h-42 object-cover justify-start  border-3 mb-8 border-amber-50'  src="https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="img" />
+        <h1 className="text-4xl font-bold">Lucky A</h1>
+        <h3 className="text-xl mt-2">Web Developer</h3>
         <p className="mt-4 text-gray-400">
           I build accessible, pixel-perfect digital experiences for the web.
         </p>
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Sidebar from "./components/Sidebar";
 
 
+
 export default function Home() {
   const userName = "Lucky";
   const mainTitle = "Welcome to the Hero Section";
